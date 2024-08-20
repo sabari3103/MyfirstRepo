848654dd22050fe9cabbe7e2486bd8183c690ac6
sabari3103 <sabari6805502@gmail.com>
2024-08-20 22:06:48 +0530

    feat : find the number whether odd or even

---
+++ gas.java
@@ -0,0 +1,13 @@
+class sabari{
+    public static void main (String []args)
+    {
+        int a1=10
+        if((a1%2)==0)
+        { 
+            system.out.Println("This given number is even");
+         }
+         else{
+            System.out.println("This given number is odd");
+         }
+    }
+}
