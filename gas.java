023daf7817066985f052760c055aec02f279f1bd
SABARINATHAN V <145964022+sabari3103@users.noreply.github.com>
2024-08-20 22:08:55 +0530

    fix : changed syntax errors

--- gas.java
+++ gas.java
@@ -1,10 +1,10 @@
 class sabari{
     public static void main (String []args)
     {
-        int a1=10
+        int a1=10;
         if((a1%2)==0)
         { 
-            system.out.Println("This given number is even");
+            System.out.println("This given number is even");
          }
          else{
             System.out.println("This given number is odd");
